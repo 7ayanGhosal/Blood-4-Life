906cef681636bef58a94fc133d8556db9fe1db6e
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-03-28 21:28:55 +0530

    connected to mongoDB Atlas, added user and hospital schema

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-var request = require('request')
-var express = require('express');
+var request = require("request");
+var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.listen(process.env.PORT||5000, process.env.IP, ()=>{
+app.listen(process.env.PORT || 5000, process.env.IP, () => {
   console.log("Server has started");
-})
-
-
-app.get("/", (req, res)=>{
-    res.redirect("https://www.google.com/");
 });
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
+//Mongoose
+mongoose.connect(
+  "mongodb+srv://Group16:bloodforlife@blood4life.i6agz.mongodb.net/Blood4LifeDB?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  surname: String,
+  email: String,
+  password: String,
+  birthday: Date,
+  zip: Number,
+  city: String,
+  address: String,
+  bloodGroup: String,
+  donor: Boolean,
+});
+
+var hospitalSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  zip: Number,
+  city: String,
+  address: String,
+  bloodGroup: Object,
+});
+
+//------------------------------------------------MODEL
+var user = mongoose.model("User", userSchema);
+var hospital = mongoose.model("Hospital", hospitalSchema);
+
+app.get("/", (req, res) => {
+  res.redirect("https://www.google.com/");
+});
