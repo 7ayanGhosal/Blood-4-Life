361c3664367b74769610dbaa95d7f7653c5c02a4
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-03-29 20:05:25 +0530

    updated navbar component

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -4,17 +4,31 @@ import classes from "./navbar.css";
 class Navbar extends Component {
   render() {
     return (
-      <div class="topnav">
-        <a class="active" href="#home">
-          Home
-        </a>
-        <a class="emergency" href="#emergency">
-          <b>Emergency</b>
-        </a>
-        <a href="#about">About us</a>
-        <a href="#contact">Contact us</a>
-        <a href="#signup">Sign Up</a>
-        <a href="#login">log in</a>
+      <div>
+        <div class="topnav">
+          <a class="active" href="#home">
+            Home
+          </a>
+          <a class="emergency" href="#emergency">
+            <b>Emergency</b>
+          </a>
+          <a href="#about">About us</a>
+          <a href="#contact">Contact us</a>
+          <a href="#signup">Sign Up</a>
+          <a href="#login">log in</a>
+        </div>
+        <div class="topnav">
+          <a class="active" href="#home">
+            Home
+          </a>
+          <a class="emergency" href="#emergency">
+            <b>Emergency</b>
+          </a>
+          <a href="#about">About us</a>
+          <a href="#contact">Contact us</a>
+          <a onclick="openNav()">Profile</a>
+          <a href="#logout">log out</a>
+        </div>
       </div>
     );
   }
