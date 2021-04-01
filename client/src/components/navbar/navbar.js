b7846d66a21e86abdfcc6288065a7b8d2e64db42
pulkit-singhania <pulkitsinghania100@gmail.com>
2021-04-02 00:05:36 +0530

    Solved hamburger display issue and improved 2nd Navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-md p-0">
+        <nav class="navbar navbar-dark navbar-expand-md p-0">
           <div class="container-fluid">
             <div class="logo">
               <img src={logo}></img>
@@ -88,20 +88,87 @@ class Navbar extends Component {
             </div>
           </div>
         </nav>
-        <div className="topnav">
-          <a className="active" href="#home">
-            Home
-          </a>
-          <a className="emergency" href="#emergency">
-            <b>Emergency</b>
-          </a>
-          <a href="#about">About us</a>
-          <a href="#contact">Contact us</a>
-          <a data-bs-toggle="modal" data-bs-target="#profileModal">
-            Profile
-          </a>
-          <a href="#logout">log out</a>
-        </div>
+        <nav class="navbar navbar-dark navbar-expand-md p-0">
+          <div class="container-fluid">
+            <div class="logo">
+              <img src={logo}></img>
+              <h3>Blood4Life</h3>
+            </div>
+            <button
+              class="navbar-toggler "
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+              <div class="icons d-flex justify-content-center">
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-5 ps-5 icons2"
+                  >
+                    <b>Emergency</b>
+                  </button>
+                </div>
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-5 ps-5 icons2"
+                  >
+                    Home
+                  </button>
+                </div>
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-4 ps-4 icons2"
+                    href="#"
+                  >
+                    About Us
+                  </button>
+                </div>
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-4 ps-4 icons2"
+                    href="#"
+                  >
+                    Contact Us
+                  </button>
+                </div>
+              </div>
+              <ul class="navbar-nav me-auto"></ul>
+              <div class="icons d-flex justify-content-end">
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-4 ps-4 icons2"
+                    href="#"
+                    data-bs-toggle="modal"
+                    data-bs-target="#profileModal"
+                  >
+                    Profile
+                  </button>
+                </div>
+                <div class="nav-item">
+                  <button
+                    type="button"
+                    class="btn navbar-custom text-align-center pe-4 ps-4 icons2"
+                    href="#"
+                  >
+                    Logout
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
