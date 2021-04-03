2e293a46d60340be86bff83507234a2795bfd1ed
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-04-03 09:38:43 +0530

    updated loginBox

--- client/src/components/loginBox/loginBox.js
+++ client/src/components/loginBox/loginBox.js
@@ -22,7 +22,7 @@ class LoginBox extends Component {
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-dialog-new1">
+          <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header special">
                 <h5 class="modal-title" id="exampleModalLabel">
