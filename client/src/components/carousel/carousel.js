3bcebbce217a3507ec4bc214a151ed7dbcacd3b1
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-04-12 12:47:23 +0530

    corrected carousel styles

--- client/src/components/carousel/carousel.js
+++ client/src/components/carousel/carousel.js
@@ -5,7 +5,7 @@ class Carousel extends Component {
   render() {
     return (
       <div>
-        <div class="carouselDiv container">
+        <div class="carouselDiv">
           <div
             id="carouselExampleIndicators"
             class="carousel slide"
@@ -69,74 +69,117 @@ class Carousel extends Component {
                 aria-label="Slide 9"
               ></button>
             </div>
-            <div class="carousel-inner">
-              <center>
-                <div class="carousel-item active">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323457.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://image.freepik.com/free-vector/illustration-people-donating-blood_52683-22259.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
+            <div class="carousel-inner ">
+              <div
+                style={{
+                  height: 60 + "vh",
 
-                <div class="carousel-item">
-                  <img
-                    src="https://image.freepik.com/free-vector/blood-cells-red-background-medical-plasma-human-artery-hemoglobin-erythrocytes-hematology-medicine-red-blood-cells-vein-stream-body-vascular-system-cancer-microbiology-science_8071-1527.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323457.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item active"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
 
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323658.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323663.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323661.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323556.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323525.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="https://wallpapercave.com/wp/wp4323468.jpg"
-                    class="d-block w-100 h-75 image"
-                    alt="..."
-                  />
-                </div>
-              </center>
+                  backgroundImage:
+                    "url('https://image.freepik.com/free-vector/illustration-people-donating-blood_52683-22259.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://image.freepik.com/free-vector/blood-cells-red-background-medical-plasma-human-artery-hemoglobin-erythrocytes-hematology-medicine-red-blood-cells-vein-stream-body-vascular-system-cancer-microbiology-science_8071-1527.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323658.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323663.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323661.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323556.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323525.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
+              <div
+                style={{
+                  height: 60 + "vh",
+
+                  backgroundImage:
+                    "url('https://wallpapercave.com/wp/wp4323468.jpg')",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                class="carousel-item"
+              ></div>
             </div>
             <button
               class="carousel-control-prev"
@@ -170,3 +213,19 @@ class Carousel extends Component {
 }
 
 export default Carousel;
+{
+  /* <div
+  style={{
+    height: 60 + "vh",
+    
+  }}
+  class="carousel-item active d-flex align-items-center justify-content-center"
+>
+  <img
+    src="https://wallpapercave.com/wp/wp4323457.jpg"
+    class="image"
+    style={{ objectFit: "cover", objectPosition: "center" }}
+    alt="..."
+  />
+</div>; */
+}
