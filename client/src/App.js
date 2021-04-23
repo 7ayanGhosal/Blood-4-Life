10586b8cec312b33e677e1dfdbb6fb6058563cfa
nervouscoder27 <adityacont@gmail.com>
2021-04-23 10:54:10 +0530

    added BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import axios from 'axios';
-import Navbar from './components/navbar/navbar';
-import Carousel from './components/carousel/carousel';
-import FooterHome from './components/footerHome/footerHome';
-import PlacePicker from './components/placePicker/placePicker';
-import Hospital from './components/hospital/hospital';
-import './App.css';
-import AuthContext from './context/auth-context';
+import React from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+import Carousel from "./components/carousel/carousel";
+import FooterHome from "./components/footerHome/footerHome";
+import PlacePicker from "./components/placePicker/placePicker";
+import Hospital from "./components/hospital/hospital";
+import "./App.css";
+import AuthContext from "./context/auth-context";
 
 class App extends React.Component {
   static contextType = AuthContext;
@@ -15,28 +16,28 @@ class App extends React.Component {
     this.state = {
       displayOTPBox: false,
       disableEmail: false,
-      name: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      pass: '',
-      gender: '',
-      age: '',
-      zip: '',
-      city: '',
-      address: '',
-      bloodGroup: '',
+      name: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      pass: "",
+      gender: "",
+      age: "",
+      zip: "",
+      city: "",
+      address: "",
+      bloodGroup: "",
       bloodStock: {
-        'A+': 0,
-        'A-': 0,
-        'B+': 0,
-        'B-': 0,
-        'AB+': 0,
-        'AB-': 0,
-        'O+': 0,
-        'O-': 0,
+        "A+": 0,
+        "A-": 0,
+        "B+": 0,
+        "B-": 0,
+        "AB+": 0,
+        "AB-": 0,
+        "O+": 0,
+        "O-": 0,
       },
-      rhFactor: '',
+      rhFactor: "",
       isHospital: false,
       reqDonor: false,
       authenticated: false,
@@ -44,26 +45,26 @@ class App extends React.Component {
 
     //CHANGE EMAIL (Changes the state)
     this.enableEmail = () => {
-      this.setState({displayOTPBox: false, disableEmail: false});
+      this.setState({ displayOTPBox: false, disableEmail: false });
     };
     //EMAIL VERIFICATION (Email has email, isHospital)
     this.onEmailSubmit = (Email) => {
-      if (Email.boxName === 'SignupBox') {
-        const body = {email: Email.email};
-        axios.post('/emailVerification', body).then(
+      if (Email.boxName === "SignupBox") {
+        const body = { email: Email.email };
+        axios.post("/emailVerification", body).then(
           (res) => {
-            if (res.data === 'Exists') {
+            if (res.data === "Exists") {
               //Already exists
-              this.setState({displayOTPBox: 'exists'});
-            } else if (res.data === 'False') {
+              this.setState({ displayOTPBox: "exists" });
+            } else if (res.data === "False") {
               //error in nodemailer
-              this.setState({displayOTPBox: 'false'});
+              this.setState({ displayOTPBox: "false" });
             } else {
               //otp sent
               //start timer, show otp box
 
               this.setState({
-                displayOTPBox: 'true',
+                displayOTPBox: "true",
                 disableEmail: true,
                 email: Email.email,
                 isHospital: Email.isHospital,
@@ -74,30 +75,30 @@ class App extends React.Component {
             console.log(error);
           }
         );
-      } else if (Email.boxName === 'ResetPassBox') {
-        const body = {email: Email.email};
+      } else if (Email.boxName === "ResetPassBox") {
+        const body = { email: Email.email };
 
-        axios.post('/resetPass/sendOTP', body).then(
+        axios.post("/resetPass/sendOTP", body).then(
           (res) => {
-            if (res.data === 'doesnotexist') {
-              document.getElementById('ResetPassOTPBox').style.display =
-                'block';
-              document.getElementById('ResetPassOTPBox').innerHTML =
+            if (res.data === "doesnotexist") {
+              document.getElementById("ResetPassOTPBox").style.display =
+                "block";
+              document.getElementById("ResetPassOTPBox").innerHTML =
                 "<h5 className='text-danger'>This Email Id doesn't exist</h5>";
             } else if (res.data === true) {
               //otp sent
               //start timer, show otp box
 
               this.context.resetPassEmail = Email.email;
-              document.getElementById('ResetPassPasswordSetter').style.display =
-                'block';
-              document.getElementById('ResetPassEmail').disabled = 'true';
-              document.getElementById('ResetPassSubmit').disabled = 'true';
+              document.getElementById("ResetPassPasswordSetter").style.display =
+                "block";
+              document.getElementById("ResetPassEmail").disabled = "true";
+              document.getElementById("ResetPassSubmit").disabled = "true";
             } else {
               //error in nodemailer
-              document.getElementById('ResetPassOTPBox').style.display =
-                'block';
-              document.getElementById('ResetPassOTPBox').innerHTML =
+              document.getElementById("ResetPassOTPBox").style.display =
+                "block";
+              document.getElementById("ResetPassOTPBox").innerHTML =
                 "<h5 className='text-danger'>Couldn't connect to server, please try again after sometime</h5>";
             }
           },
@@ -110,34 +111,34 @@ class App extends React.Component {
 
     //OTP VERIFICATION
     this.onOTPSubmit = (OTP) => {
-      const body = {otp: OTP.otp};
-      axios.post('/otpVerification', body).then(
+      const body = { otp: OTP.otp };
+      axios.post("/otpVerification", body).then(
         (res) => {
-          if (res.data === 'False') {
+          if (res.data === "False") {
             //Wrong OTP
-            console.log('INVALID OTP');
+            console.log("INVALID OTP");
           } else {
             //correct OTP
             //turn off signupbox
-            document.getElementById('closeSignupBox').click();
+            document.getElementById("closeSignupBox").click();
             //turn on passwordSetter
-            document.getElementById('passwordSetterModalButton').click();
+            document.getElementById("passwordSetterModalButton").click();
           }
         },
         (error) => {
-          console.log('ERROR IN OTP ONSUBMIT, App.js');
+          console.log("ERROR IN OTP ONSUBMIT, App.js");
         }
       );
     };
 
     //password matching
     this.setPassword = (pass) => {
-      this.setState({pass: pass});
+      this.setState({ pass: pass });
       //same passwords
       //turn off passwordSetter
-      document.getElementById('closePasswordBox').click();
+      document.getElementById("closePasswordBox").click();
       //turn on profileSetter
-      document.getElementById('profileSetterModalButton').click();
+      document.getElementById("profileSetterModalButton").click();
     };
     //profileSetter
     this.setProfile = async (profile) => {
@@ -154,49 +155,53 @@ class App extends React.Component {
         rhFactor: profile.rhFactor,
         reqDonor: profile.reqDonor,
       });
-      axios.post('/signup', this.state).then(
+      axios.post("/signup", this.state).then(
         (res) => {
           if (res.data) {
-            document.getElementById('closeProfileSetterModal').click();
-            this.setState({authenticated: true});
+            document.getElementById("closeProfileSetterModal").click();
+            this.setState({ authenticated: true });
+            if (this.state.isHospital) window.location.href = "/hospital";
+            else window.location.href = "/user";
           } else
             console.log(
-              'from App.js, there is some error in index.js(backend)'
+              "from App.js, there is some error in index.js(backend)"
             );
         },
         (error) => {
-          console.log('app.js: Error in /signup' + error);
+          console.log("app.js: Error in /signup" + error);
         }
       );
     };
     //PROFILE RESET
     this.resetProfile = (profile) => {
-      axios.post('/resetprofile', profile).then((res) => {
+      axios.post("/resetprofile", profile).then((res) => {
         if (!res.data) {
-          console.log('ERROR IN CHANGING VALUE!!');
+          console.log("ERROR IN CHANGING VALUE!!");
         } else {
-          this.setState({...profile});
-          document.getElementById('closeProfileResetterModal').click();
+          this.setState({ ...profile });
+          document.getElementById("closeProfileResetterModal").click();
         }
       });
     };
     //LOGIN ROUTE
     this.checkLogin = (cred) => {
-      axios.post('/login', cred).then((res) => {
+      axios.post("/login", cred).then((res) => {
         if (!res.data) {
-          document.getElementById('loginMessage').innerHTML =
+          document.getElementById("loginMessage").innerHTML =
             "<h5 className='text-danger'>Incorrect Details!</h5>";
         } else {
           // document.getElementById("loginMessage").innerHTML =
           //   "<h5 className='text-danger'>Logging In...</h5>";
           var IsHospital = false;
           if (res.data.name) IsHospital = true;
-          document.getElementById('closeLoginModal').click();
+          document.getElementById("closeLoginModal").click();
           this.setState({
             authenticated: true,
             ...res.data,
             isHospital: IsHospital,
           });
+          if (IsHospital) window.location.href = "/hospital";
+          else window.location.href = "/user";
         }
       });
     };
@@ -205,17 +210,17 @@ class App extends React.Component {
       this.setState({
         displayOTPBox: false,
         disableEmail: false,
-        firstName: '',
-        lastName: '',
-        email: '',
-        pass: '',
-        gender: '',
-        age: '',
-        address: '',
-        bloodGroup: '',
-        rhFactor: '',
+        firstName: "",
+        lastName: "",
+        email: "",
+        pass: "",
+        gender: "",
+        age: "",
+        address: "",
+        bloodGroup: "",
+        rhFactor: "",
         isHospital: false,
-        reqDonor: '',
+        reqDonor: "",
         authenticated: false,
       });
     };
@@ -230,8 +235,8 @@ class App extends React.Component {
     //Passsword Reset Route
     this.checkResetPassword = (pass) => {
       this.context.resetPassPassword = pass;
-      document.getElementById('ResetPassOTPBox').style.display = 'block';
-      document.getElementById('ResetPassPasswordSetter').style.display = 'none';
+      document.getElementById("ResetPassOTPBox").style.display = "block";
+      document.getElementById("ResetPassPasswordSetter").style.display = "none";
     };
     this.resetPassword = (OTP) => {
       const body = {
@@ -239,16 +244,16 @@ class App extends React.Component {
         password: this.context.resetPassPassword,
         otp: OTP.otp,
       };
-      axios.post('/resetPass/otpVerification', body).then(
+      axios.post("/resetPass/otpVerification", body).then(
         (res) => {
-          if (res.data === 'InvalidOTP') {
+          if (res.data === "InvalidOTP") {
             //Wrong OTP
-            document.getElementById('ResetPassOTPBoxMessage').innerText =
-              'Invalid OTP!!!';
+            document.getElementById("ResetPassOTPBoxMessage").innerText =
+              "Invalid OTP!!!";
           } else {
             //correct OTP
             //this.setState({ displayOTPBox: false, disableEmail: false });
-            document.getElementById('closeResetPassBox').click();
+            document.getElementById("closeResetPassBox").click();
             var IsHospital = false;
             if (res.data.name) IsHospital = true;
             this.setState({
@@ -256,10 +261,12 @@ class App extends React.Component {
               ...res.data,
               isHospital: IsHospital,
             });
+            if (IsHospital) window.location.href = "/hospital";
+            else window.location.href = "/user";
           }
         },
         (error) => {
-          console.log('ERROR IN OTP ONSUBMIT, App.js' + error);
+          console.log("ERROR IN OTP ONSUBMIT, App.js" + error);
         }
       );
     };
@@ -270,29 +277,31 @@ class App extends React.Component {
       <div>
         <img class="bodyImg" src="https://wallpapercave.com/wp/wp4323580.png" />
         <div class="bodyImgColor" />
-        <AuthContext.Provider
-          value={{
-            ...this.state,
-            onEmailSubmit: this.onEmailSubmit,
-            onOTPSubmit: this.onOTPSubmit,
-            enableEmail: this.enableEmail,
-            setPassword: this.setPassword,
-            setProfile: this.setProfile,
-            checkLogin: this.checkLogin,
-            logout: this.logout,
-            remove: this.remove,
-            resetProfile: this.resetProfile,
-            checkResetPassword: this.checkResetPassword,
-            resetPassword: this.resetPassword,
-          }}
-        >
-          <Navbar></Navbar>
-        </AuthContext.Provider>
-        <Carousel></Carousel>
 
-        <Hospital></Hospital>
-        <PlacePicker></PlacePicker>
-        <FooterHome></FooterHome>
+        <div>
+          <AuthContext.Provider
+            value={{
+              ...this.state,
+              onEmailSubmit: this.onEmailSubmit,
+              onOTPSubmit: this.onOTPSubmit,
+              enableEmail: this.enableEmail,
+              setPassword: this.setPassword,
+              setProfile: this.setProfile,
+              checkLogin: this.checkLogin,
+              logout: this.logout,
+              remove: this.remove,
+              resetProfile: this.resetProfile,
+              checkResetPassword: this.checkResetPassword,
+              resetPassword: this.resetPassword,
+            }}
+          >
+            <Navbar></Navbar>
+            <Route exact path="/" component={Carousel} />
+            <Route path="/hospital" component={Hospital} />
+            <PlacePicker></PlacePicker>
+            <FooterHome></FooterHome>
+          </AuthContext.Provider>
+        </div>
       </div>
     );
   }
