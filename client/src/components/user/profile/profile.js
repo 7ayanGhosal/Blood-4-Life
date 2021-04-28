01686dad463605b4ebd39e659dc7bcb8feeca03b
nervouscoder27 <adityacont@gmail.com>
2021-04-28 21:07:09 +0530

    added address display functionality in profile section

--- client/src/components/user/profile/profile.js
+++ client/src/components/user/profile/profile.js
@@ -151,6 +151,24 @@ class Profile extends React.Component {
             </h3>
             <br />
             <h3 class="attribute" href="#">
+              Address :-{" "}
+            </h3>
+            &ensp;
+            <h3 class="value">
+              {this.context.location.poi +
+                this.context.location.street +
+                this.context.location.subSubLocality +
+                this.context.location.subLocality +
+                this.context.location.locality +
+                this.context.location.village +
+                this.context.location.district +
+                this.context.location.subDistrict +
+                this.context.location.city +
+                this.context.location.state +
+                this.context.location.pincode}
+            </h3>
+            <br />
+            <h3 class="attribute" href="#">
               Gender :-{" "}
             </h3>
             &ensp;
