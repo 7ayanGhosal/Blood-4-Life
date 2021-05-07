e15ab2b59a90f81bca7fe6992c9a7ea2e68858a8
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-05-07 12:56:51 +0530

    Emergency Hospital List hidden

--- client/src/components/emergency/emergency.js
+++ client/src/components/emergency/emergency.js
@@ -9,6 +9,7 @@ class Emergency extends React.Component {
     super(props);
     this.state = {
       Hospitals: [],
+      dispState: 1, // 1. default, 2. found Hosp 3. No Hosp
       details: {
         name: "",
         contact: "",
@@ -19,15 +20,25 @@ class Emergency extends React.Component {
     this.hospSearch = (data) => {
       axios.post("/emergency", data).then(
         (Res) => {
-          this.setState({
-            details: {
-              name: data.details.name,
-              contact: data.details.contact,
-              bloodGroup: data.details.bloodGroup,
-              rhFactor: data.details.rhFactor,
+          // console.log(Res);
+          this.setState(
+            {
+              details: {
+                name: data.details.name,
+                contact: data.details.contact,
+                bloodGroup: data.details.bloodGroup,
+                rhFactor: data.details.rhFactor,
+              },
+              Hospitals: Res.data,
             },
-            Hospitals: Res.data,
-          });
+            () => {
+              if (Res.data.length === 0) {
+                this.setState({ dispState: 3 });
+              } else {
+                this.setState({ dispState: 2 });
+              }
+            }
+          );
         },
         (err) => {
           console.log(err);
@@ -36,17 +47,17 @@ class Emergency extends React.Component {
     };
   }
   render() {
-    var box = (
-      <HospitalList
-        list={this.state.Hospitals}
-        details={this.state.details}
-      ></HospitalList>
-    );
     return (
       <div>
         <h1>Emergency</h1>
         <PlacePicker hospSearch={this.hospSearch}></PlacePicker>
-        {box}
+        {this.state.dispState !== 1 ? (
+          <HospitalList
+            list={this.state.Hospitals}
+            details={this.state.details}
+            dispState={this.state.dispState}
+          ></HospitalList>
+        ) : null}
       </div>
     );
   }
